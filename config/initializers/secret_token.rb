e6ff4d786a7ceaa11f9086cac431afe5e1762755
Stephanie Datu <stephdatu@gmail.com>
2013-07-24 15:29:57 -0700

    Generate a tutor-student matchmaking application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TutorMatch::Application.config.secret_key_base = '29300029da4c214a141a1ffdc08a2ea4e5ce5c4d329f1b97b56caf26773975d60a7253c73948c87409165e61f9dbc3bd77bbf81d8414f468772a857f8fcac446'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TutorMatch::Application.config.session_store :cookie_store, key: '_tutor_match_session'
